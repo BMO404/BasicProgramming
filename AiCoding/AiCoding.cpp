2241271a0a5ae40138b34f9e0d0dd1919e57f7bb
pks <pkwangseong@gmail.com>
2026-04-07 11:21:36 +0900

    AiCoding 이중 for 문 do while 문

--- AiCoding/AiCoding.cpp
+++ AiCoding/AiCoding.cpp
@@ -1,62 +1,80 @@
 #include <cstdio>
 
-int main()
-{
-    printf("=== C++ 반복문 예제 ===\n\n");
+int main() {
+    printf("=== C++ 이중 반복문과 do-while문 예제 ===\n\n");
 
-    // for문 예제: 1부터 5까지 출력
-    printf("[for문: 1부터 5까지]\n");
-    for (int i = 1; i <= 5; i++) {
-        printf("%d ", i);
-    }
-    printf("\n\n");
-
-    // for문 + 조건문: 짝수만 출력
-    printf("[for문 + 조건문: 1부터 10까지 중 짝수만]\n");
-    for (int i = 1; i <= 10; i++) {
-        if (i % 2 == 0) {
-            printf("%d ", i);
+    // 이중 for문: 구구단 표
+    printf("[이중 for문: 2단과 3단 구구단]\n");
+    for (int dan = 2; dan <= 3; dan++) {
+        printf("<%d단>\n", dan);
+        for (int i = 1; i <= 9; i++) {
+            printf("%d x %d = %d\n", dan, i, dan * i);
         }
+        printf("\n");
     }
-    printf("\n\n");
 
-    // while문 예제: 5부터 1까지 역순 출력
-    printf("[while문: 5부터 1까지 역순]\n");
-    int count = 5;
-    while (count >= 1) {
-        printf("%d ", count);
-        count--;
-    }
-    printf("\n\n");
-
-    // while문 + 조건문: 합계 계산
-    printf("[while문 + 조건문: 1부터 100까지 합계]\n");
-    int sum = 0;
-    int num = 1;
-    while (num <= 100) {
-        sum += num;
-        num++;
-    }
-    printf("합계: %d\n\n", sum);
-
-    // for문 + 조건문: 구구단 2단
-    printf("[for문 + 조건문: 2단 (짝수 결과만)]\n");
-    for (int i = 1; i <= 9; i++) {
-        int result = 2 * i;
-        if (result % 2 == 0) {
-            printf("2 x %d = %d\n", i, result);
+    // 이중 for문 + 조건문: 9x9 구구단 (5 이상만)
+    printf("[이중 for문 + 조건문: 결과가 5 이상인 것만]\n");
+    for (int i = 1; i <= 3; i++) {
+        for (int j = 1; j <= 9; j++) {
+            int result = i * j;
+            if (result >= 5) {
+                printf("%d*%d=%d  ", i, j, result);
+            }
         }
+        printf("\n");
     }
     printf("\n");
 
-    // for문: 중첩 반복문 (별 패턴)
-    printf("[중첩 for문: 별 패턴]\n");
+    // 이중 for문: 직사각형 패턴
+    printf("[이중 for문: 직사각형 패턴]\n");
     for (int i = 1; i <= 3; i++) {
+        for (int j = 1; j <= 4; j++) {
+            printf("# ");
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // 이중 for문: 삼각형 패턴
+    printf("[이중 for문: 삼각형 패턴]\n");
+    for (int i = 1; i <= 5; i++) {
         for (int j = 1; j <= i; j++) {
             printf("* ");
         }
         printf("\n");
     }
+    printf("\n");
+
+    // do-while문: 카운트 다운
+    printf("[do-while문: 카운트 다운]\n");
+    int countdown = 5;
+    do {
+        printf("%d\n", countdown);
+        countdown--;
+    } while (countdown > 0);
+    printf("\n");
+
+    // do-while문 + 조건문: 합계 계산
+    printf("[do-while문 + 조건문: 1부터 50까지 중 3의 배수만 합계]\n");
+    int count = 1;
+    int total = 0;
+    do {
+        if (count % 3 == 0) {
+            total += count;
+            printf("%d ", count);
+        }
+        count++;
+    } while (count <= 50);
+    printf("\n합계: %d\n\n", total);
+
+    // do-while문: 사용자 입력 시뮬레이션
+    printf("[do-while문: 숫자 출력]\n");
+    int num = 1;
+    do {
+        printf("숫자: %d\n", num);
+        num++;
+    } while (num <= 3);
 
     return 0;
 }
