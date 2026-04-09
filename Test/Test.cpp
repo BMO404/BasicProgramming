26c86d530bd6a044541fdd081a46ed050b8bb4e3
pks <pkwangseong@gmail.com>
2026-04-09 09:22:35 +0900

    Test File

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,8 +1,19 @@
 #include <cstdio>
 #define SAFE_DELETE(p) if (p) { delete p; p = nullptr; }
 
+int Myprint()
+{
+    printf("Myprint\n");
+    return 1;
+}
+
+
 int main(int argc, char* argv[])
 {
+    
+    int p = Myprint();
+    printf("%d\n", p);
+    
     char x = 'A';
     int i;
     
--- gugudan/gugudan.cpp
+++ gugudan/gugudan.cpp
@@ -1,14 +1,27 @@
 #include <cstdio>
 
+double Add (double a, double b)
+{
+    return a + b;
+}
+
+double Mul(double a, double b)
+{
+    return a * b;
+}
+
+double Div(double a, double b)
+{
+    return a / b;
+}
+
+double Sub(double a, double b)
+{
+    return a - b;
+}
+
 int main() {
-    // 구구단 출력 (2단부터 9단까지)
-    for (int i = 2; i <= 9; ++i) {
-        printf("=== %d단 ===\n", i);
-        for (int j = 1; j <= 9; ++j) {
-            printf("%d × %d = %d\n", i, j, i * j);
-        }
-        printf("\n");
-    }
+    
     
     return 0;
 }
